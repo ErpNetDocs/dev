53f549c8c60b75836f58c7443f28d43734fd8648
Dilyan Rusev <dilyanrusev@gmail.com>
2020-09-24 16:17:48 +0300

    more debugging, but only if flag is specified

--- build.ps1
+++ build.ps1
@@ -1,20 +1,26 @@
 param(
     [string]$docfx,
-    [string]$remote
+    [string]$remote,
+    [switch]$debug
 )
 
-#$git_bin = "C:\Program Files\Git\usr\bin"
+if ($debug -eq $true) {
+    #$git_bin = "C:\Program Files\Git\usr\bin"
+    $working_directory = Get-Location | Out-String
+    Write-Host "cwd is $working_directory"
+    Write-Host "user is $env:USERNAME"
 
-Write-Host "user is $env:USERNAME"
-
-if (test-path "$home\.ssh\id_rsa.pub") {
-    Write-Host "Has public key:"
-    Get-Content "$home\.ssh\id_rsa.pub"
-} else {
-    Write-Host "Doesn't have public key"
+    if (test-path "$home\.ssh\id_rsa.pub") {
+        Write-Host "Has public key:"
+        Get-Content "$home\.ssh\id_rsa.pub"
+    }
+    else {
+        Write-Host "Doesn't have SSH public key!!"
+    }
 }
 
-Write-Host Setting up git remote ssh...
+
+Write-Host "Setting up git remote ssh..."
 
 git config remote.ssh.url
 if ($lastexitcode -eq 1) {
@@ -22,12 +28,12 @@ if ($lastexitcode -eq 1) {
 }
 
 git checkout master
-Write-Host Undoing local changes...
+Write-Host "Undoing local changes..."
 git reset --hard HEAD^
 
-Write-Host Building docfx
+Write-Host "Building docfx"
 Invoke-Expression "$docfx build"
 
-Write-Host Upload Changes to Github
+Write-Host "Upload Changes to Github"
 git add -A
 git push ssh master
